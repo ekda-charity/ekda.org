f0bd16238fb5bfd3415d5a3c4509da81d8e5d40c
rahman <rahman@Desktop>
2015-08-15 08:59:17 +0100

    Added code to accept donations from amic

--- module/Application/src/Application/Controller/QurbaniApiController.php
+++ module/Application/src/Application/Controller/QurbaniApiController.php
@@ -55,17 +55,49 @@ namespace Application\Controller {
             }
         }
         
+        public function checkstockandinitiatedonationfromamic() {
+            try {
+                $jsonData = $this->getRequest()->getContent();
+                $data = $this->serializer->deserialize($jsonData, "Application\API\Canonicals\Entity\Qurbani", "json");
+
+                $qurbaniRepo = $this->getServiceLocator()->get('QurbaniRepo');
+                $config = $this->getServiceLocator()->get('Config');
+
+                $exitDomainname = $this->params()->fromRoute('p1');
+                $apiDomainname = $config["DomainName"];
+                $qurbaniDetails = $qurbaniRepo->getQurbaniDetails();
+                $qurbanikey = $qurbaniRepo->checkStockAndAddQurbani($data);
+                
+                $shortUrl = $qurbaniDetails->shorturl;
+                $amount = $data->getTotal();
+                $exitUrl = "http://$apiDomainname/api/QurbaniApi/confirmdonation/JUSTGIVING-DONATION-ID/$qurbanikey/$exitDomainname";
+                $redirectUrl = "http://www.justgiving.com/$shortUrl/4w350m3/donate?amount=$amount&exitUrl=$exitUrl";
+
+                $response = ResponseUtils::createSingleFetchResponse($redirectUrl);
+                return $this->jsonResponse($response);
+                
+            } catch (\Exception $ex) {
+                $response = ResponseUtils::createExceptionResponse($ex);
+                return $this->jsonResponse($response);
+            }
+        }
+        
         public function confirmdonationAction() {
             try {
                 $donationId = $this->params()->fromRoute('p1');
                 $qurbanikey = $this->params()->fromRoute('p2');
+                $exitDomainname = $this->params()->fromRoute('p3');
                 
                 $qurbaniRepo = $this->getServiceLocator()->get('QurbaniRepo');
                 $qurbani = $qurbaniRepo->confirmDonation($qurbanikey, $donationId);
-                $donation = $qurbani->getSheep() . " sheep, " . $qurbani->getCows() . " cows, " . $qurbani->getCamels() . " camels";
-
-                $this->flashMessenger()->addSuccessMessage("Your Donation of $donation completed Successfully. May Allah reward you generously. Amin.");
-                return $this->redirect()->toUrl("/");
+                
+                if (isset($exitDomainname)) {
+                    return $this->redirect()->toUrl("http://$exitDomainname");
+                } else {
+                    $donation = $qurbani->getSheep() . " sheep, " . $qurbani->getCows() . " cows, " . $qurbani->getCamels() . " camels";
+                    $this->flashMessenger()->addSuccessMessage("Your Donation of $donation completed Successfully. May Allah reward you generously. Amin.");
+                    return $this->redirect()->toUrl("/");
+                }
                 
             } catch (\Exception $ex) {
                 $this->flashMessenger()->addErrorMessage("There was a problem with your donation: " . $ex->getMessage());
