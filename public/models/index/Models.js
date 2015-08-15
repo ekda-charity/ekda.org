2ff24c4fdb0e502638c4a52cb5e1425926e1226a
rahman <rahman@Desktop>
2015-08-16 00:16:10 +0100

    Changed the alert to state instructions rather than names to be mentioned. Also added auto completion for the form

--- public/models/index/Models.js
+++ public/models/index/Models.js
@@ -56,7 +56,7 @@ namespace("ekda.index").Qurbani = function (data, confirmDonation) {
             }
             
             if (!self.instructions()) {
-                errors.push("Names to mention is required")
+                errors.push("Instructions are required")
             }
         }
 
