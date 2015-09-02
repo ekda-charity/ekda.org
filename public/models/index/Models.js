5f2ff318eff33ad78472da7775498f8cd34df4a4
rahman <rahman@Desktop>
2015-09-02 23:31:07 +0100

    Changed the manner in which details are taken from the user

--- public/models/index/Models.js
+++ public/models/index/Models.js
@@ -42,7 +42,7 @@ namespace("ekda.index").Qurbani = function (data, confirmDonation) {
     self.total = ko.computed(function () {
         return self.sheepTotal() + self.cowsTotal() + self.camelsTotal();
     });
-    
+
     self.donate = function (data, e) {
         
         var errors = [];
@@ -50,47 +50,78 @@ namespace("ekda.index").Qurbani = function (data, confirmDonation) {
         if (self.total() <= 0) {
             errors.push("At least one animal is required");
         }
-        
-        if (self.addInstructions()) {
-            if (!self.instructions()) {
-                errors.push("Instructions are required")
-            }
-        }
 
         if (errors.length > 0) {
             toastrErrorFromList(errors, "Validation Errors");
             return;
         } else {
-            var url = "/api/QurbaniApi/" + (self.confirmDonation ? "checkstockanddonate" : "checkstockandinitiatedonation");
-            var obj = ko.toJSON({
-                sheep: self.sheep(),
-                cows: self.cows(),
-                camels: self.camels(),
-                total: self.total(),
-                fullname: self.fullname(),
-                email: self.email(),
-                mobile: self.mobile(),
-                instructions: self.instructions()
-            });
             
-            var lad = Ladda.create(e.currentTarget);
-            lad.start();
-        
-            ajaxPost(url, obj, function (response) {
-                lad.stop();
-                if (!response.success) {
-                    toastrErrorFromList(response.errors, "Validation Failed");
-                } else if (self.confirmDonation) {
-                    self.sheep(null);
-                    self.cows(null);
-                    self.camels(null);
-                    self.fullname(null);
-                    self.email(null);
-                    self.mobile(null);
-                    self.instructions(null);
-                    toastrSuccess("Donation was successfull");
-                } else {
-                    document.location.href = response.item;
+            var message = '<div class="row">  ' +
+                '<div class="col-md-12"> ' +
+                    '<form class="form-horizontal"> ' +
+                        '<div class="form-group"> ' +
+                            '<div class="col-md-12"> ' +
+                                (self.disableInstructions() ? '' : '<textarea type="text" id="qurbaniName" name="name" class="form-control input-md" rows="3" placeholder="Names (Optional)"></textarea>') +
+                                '<input id="qurbaniEmail" name="email" type="text" placeholder="Email (To alert you after Qurbani)" class="form-control input-md top5"> ' +
+                            '</div> ' +
+                        '</div> ' +
+                    '</form>' +
+                '</div> ' +
+            '</div> ';
+
+            bootbox.dialog({
+                title: "On behalf of ?",
+                message: message,
+                buttons: {
+                    cancel: {
+                        label: "Cancel",
+                        className: "btn-default",
+                        callback: function () {
+                        }
+                    },
+                    success: {
+                        label: "Continue",
+                        className: "btn-primary",
+                        callback: function () {
+                            var name = $('#qurbaniName').val();
+                            var email = $('#qurbaniEmail').val();
+                            self.instructions(name || null);
+                            self.email(email || null);
+                            
+                            var url = "/api/QurbaniApi/" + (self.confirmDonation ? "checkstockanddonate" : "checkstockandinitiatedonation");
+                            var obj = ko.toJSON({
+                                sheep: self.sheep(),
+                                cows: self.cows(),
+                                camels: self.camels(),
+                                total: self.total(),
+                                fullname: self.fullname(),
+                                email: self.email(),
+                                mobile: self.mobile(),
+                                instructions: self.instructions()
+                            });
+
+                            var lad = Ladda.create(e.currentTarget);
+                            lad.start();
+
+                            ajaxPost(url, obj, function (response) {
+                                lad.stop();
+                                if (!response.success) {
+                                    toastrErrorFromList(response.errors, "Validation Failed");
+                                } else if (self.confirmDonation) {
+                                    self.sheep(null);
+                                    self.cows(null);
+                                    self.camels(null);
+                                    self.fullname(null);
+                                    self.email(null);
+                                    self.mobile(null);
+                                    self.instructions(null);
+                                    toastrSuccess("Donation was successfull");
+                                } else {
+                                    document.location.href = response.item;
+                                }
+                            });        
+                        }
+                    }
                 }
             });
         }
